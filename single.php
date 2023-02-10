18b2fb2c550ba625ced207996dba11b1997072cd
Tom Cooper <58119780+TomCooper01@users.noreply.github.com>
2023-02-10 18:00:51 +0000

    added author meta

--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <header class="page-title theme-bg-light text-center py-5">
-<h1 class="heading"><?php the_title(); ?></h1>
+<h1 class="page-title"><?php the_title(); ?> By <?php echo get_the_author_meta('user_login', $post->post_author); ?></h1>
 <h2 class="Date"><?php echo get_the_date('F j, Y'); ?></h3>
 </header>
 <article class="content px-3 py-5 p-md-5">
