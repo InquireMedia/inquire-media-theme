60c90d6951cc55c48504ceac2e2c47c27ef616d5
Tom Cooper <58119780+TomCooper01@users.noreply.github.com>
2023-02-20 00:41:07 +0000

    Header updated

--- header.php
+++ header.php
@@ -7,40 +7,38 @@
 </head> 
 
 <body>
-    <header class="header d-flex align-items-center">    
-		<?php
-			if(function_exists('the_custom_logo')){ 
-				$custom_log_id = get_theme_mod('custom_logo');
-				$logo = wp_get_attachment_image_src($custom_log_id);
-			}
-		?>
-		<img class="mb-3 mx-4 logo" src="<?php echo $logo[0]?>" alt="logo" >
-	    <a class="site-title" href="index.html">		
-			<?php echo get_bloginfo('name'); ?>
-		</a>
-        
-	    <nav class="navbar navbar-expand-lg navbar-dark d-flex align-items-center" >
-			<button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-		
-
-			<div id="navigation" class="collapse navbar-collapse flex-column">
+    <header class="header d-flex align-items-center">
+		<div class="container">
+	    	<nav class="navbar navbar-expand-lg navbar-dark d-flex align-items-center" >
+				<button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+	
 				<?php
-					wp_nav_menu(
-						array(
-							'menu' => 'primary',
-							'container' => '',
-							'theme_location' => 'primary',
-							'items_wrap' => '<ul id="" class="navbar-nav ml-auto">%3$s</ul>' //  d-flex align-items-center
-						)
-					);
+					if(function_exists('the_custom_logo')){ 
+						$custom_log_id = get_theme_mod('custom_logo');
+						$logo = wp_get_attachment_image_src($custom_log_id);
+					}
 				?>
-				<?php
-					dynamic_sidebar('sidebar-1');
-				?>
-			</div>
-		</nav>
+				<img class="mb-3 mx-4 logo" src="<?php echo $logo[0]?>" alt="logo" >
+	    		<a class="site-title" href=<?php echo home_url(); ?>>		
+					<?php echo get_bloginfo('name'); ?>
+				</a>
+				
+				<div id="navigation" class="collapse navbar-collapse flex-column">
+					<?php
+						wp_nav_menu(
+							array(
+								'menu' => 'primary',
+								'container' => '',
+								'theme_location' => 'primary',
+								'items_wrap' => '<ul id="" class="navbar-nav ml-auto">%3$s</ul>' //  d-flex align-items-center
+							)
+						);
+					?>
+					<?php dynamic_sidebar('sidebar-1'); ?>
+				</div>
+			</nav>
+		</div>
 	</header>
 		
-</header>
