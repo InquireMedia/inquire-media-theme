20ce7f3b93d3c611acb77e0b0534d40bb00cdbf0
Tom Cooper <58119780+TomCooper01@users.noreply.github.com>
2023-03-20 10:26:30 +0000

    Added title to section headpage

--- archive.php
+++ archive.php
@@ -4,6 +4,10 @@
 	<div id="main" class="container">
 		<div class="col-12 news-container">
 			<div class="col-12">
+				<hr class="news-section-hr <?php single_cat_title(); ?>-Bar">
+				<a class="news-section-bar <?php single_cat_title(); ?>" href="<?php echo get_category_link(get_cat_ID(single_cat_title('', false))); ?>">
+					<p class="news-section-text-option"><?php single_cat_title(); ?> </p>
+				</a>
 				<div id="main-placeholder" class="row mainOpinion">
 					<?php
 					if ( have_posts() ) {
